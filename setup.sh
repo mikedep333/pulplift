f7fd878ebdff10215c2cac504858b817b0a273b1
Mike DePaulo <mikedep333@redhat.com>
2019-10-11 09:58:08 -0400

    Add another command

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 ansible-galaxy install pulp.pulp_rpm_prerequisites -p ./playbooks/roles/
+git clone https://github.com/pulp/pulpcore.git ../pulpcore
 git submodule update --init
 vagrant up pulp3-source-fedora30
